eb69b94a0556c7e64c4ad8832b1750dc761f68aa
Shea Kelly <shea.kelly@lwb.org.au>
2020-12-05 17:07:47 +1100

    finished day 3

--- src/Day03.hs
+++ src/Day03.hs
@@ -18,8 +18,8 @@ data Coord = Coord {x :: Int, y :: Int} deriving (Show)
 
 data Slope = Slope {right :: Int, down :: Int}
 
-move :: Int -> Int -> String -> Coord -> Coord
-move right down row (Coord {x, y}) =
+move :: Slope -> String -> Coord -> Coord
+move (Slope {right, down}) row (Coord {x, y}) =
   Coord {x = adjusted, y = y + down}
   where
     x' = x + right
@@ -32,33 +32,33 @@ isTree _ = False
 isTreeAt :: Int -> String -> Bool
 isTreeAt i row = isTree $ row !! i
 
-traverseMap :: String -> Slope -> Int
-traverseMap content (Slope {down, right}) =
-  foldl
-    ( \(coord@(Coord {x, y}), trees) row ->
-        ( move right down row coord,
-          if isTreeAt x row
-            then trees + 1
-            else trees
-        )
-    )
-    (Coord 0 0, 0)
-    map
-    & snd
+traverseMap' :: Coord -> [String] -> Int -> Slope -> Int
+traverseMap' coord@(Coord {x, y}) map trees slope
+  | y > length map - 1 = trees
+  | otherwise = traverseMap' coord' map trees' slope
   where
-    map = lines content
+    row = map !! y
+    coord' = move slope row coord
+    trees' =
+      if isTreeAt x row
+        then trees + 1
+        else trees
+
+traverseMap :: String -> Slope -> Int
+traverseMap input slope =
+  traverseMap' (Coord 0 0) (lines input) 0 slope
 
 part01 :: String -> Int
-part01 content =
-  traverseMap content (Slope 3 1)
+part01 input =
+  traverseMap input (Slope 3 1)
 
 part02 :: String -> Int
-part02 content =
-  traverseMap content <$> [Slope 1 1, Slope 3 1, Slope 5 1, Slope 7 1, Slope 1 2]
+part02 input =
+  traverseMap input <$> [Slope 1 1, Slope 3 1, Slope 5 1, Slope 7 1, Slope 1 2]
     & product
 
 main :: IO ()
 main = do
-  content <- readFile "./src/Day03.txt"
-  print $ part01 content
-  print $ part02 content
+  input <- readFile "./src/Day03.txt"
+  print $ part01 input
+  print $ part02 input
