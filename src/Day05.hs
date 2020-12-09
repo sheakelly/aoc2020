870467c97bed10d2c7a465982658b200a2035f6b
Shea Kelly <shea.kelly@lwb.org.au>
2020-12-09 12:58:17 +1100

    day 6 complete

--- src/Day05.hs
+++ src/Day05.hs
@@ -3,6 +3,7 @@
 
 module Main where
 
+import Control.Applicative
 import Control.Monad
 import Data.Function ((&))
 import Data.List
@@ -15,27 +16,40 @@ import Prelude hiding (lines, max, min)
 keep :: Char -> [Int] -> [Int]
 keep 'F' xs = fst $ splitAt (length xs `div` 2) xs
 keep 'B' xs = snd $ splitAt (length xs `div` 2) xs
+keep 'L' xs = fst $ splitAt (length xs `div` 2) xs
+keep 'R' xs = snd $ splitAt (length xs `div` 2) xs
 keep _ xs = xs
 
 row :: String -> Int
 row seat =
-  foldl (\acc cur -> keep cur acc) rows (take 7 seat) !! 0
-  where
-    rows = [0 .. 127]
+  head $ foldl (\acc cur -> keep cur acc) [0 .. 127] (take 7 seat)
 
-part01 :: String -> [Int]
-part01 input =
-  map row seats
+column :: String -> Int
+column seat =
+  head $ foldl (\acc cur -> keep cur acc) [0 .. 7] (drop 7 seat)
+
+calcSeatId :: (Int, Int) -> Int
+calcSeatId (row, column) = row * 8 + column
+
+calcSeatIds :: String -> [Int]
+calcSeatIds input =
+  map (liftA2 (,) row column) seats
+    & map calcSeatId
   where
     seats = lines input
 
+part01 :: String -> Int
+part01 input =
+  maximum $ calcSeatIds input
+
 part02 :: String -> Int
-part02 input = 0
+part02 input =
+  head $ [minimum seatIds .. maximum seatIds] \\ sort seatIds
+  where
+    seatIds = calcSeatIds input
 
 main :: IO ()
 main = do
   input <- readFile "./src/Day05.txt"
   print $ part01 input
   print $ part02 input
-
-{-print $ part01 input-}
